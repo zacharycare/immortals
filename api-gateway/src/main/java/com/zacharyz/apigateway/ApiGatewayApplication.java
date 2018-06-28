08775b0b5b779f5f9bc76a2e5adfbe16978e0415
zachary.care@protonmail.com <z930g329q>
2018-06-28 11:29:30 +0800

    Initial commit

--- api-gateway/src/main/java/com/zacharyz/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/zacharyz/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,20 @@
 package com.zacharyz.apigateway;
 
+import com.zacharyz.apigateway.configuration.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class,args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
---
+++ api-gateway/src/main/java/com/zacharyz/apigateway/configuration/AccessFilter.java
@@ -0,0 +1,36 @@
+package com.zacharyz.apigateway.configuration;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String accessToken = request.getParameter("accessToken");
+        if (accessToken == null){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+        }
+        return null;
+    }
+}
